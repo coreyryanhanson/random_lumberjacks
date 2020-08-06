25a82769f57f5cff073859a38b25251cd2be2967
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-08-06 11:44:30 -0400

    added pytorch dockerfile

--- env-dockerfiles/update_dockerfiles.py
+++ env-dockerfiles/update_dockerfiles.py
@@ -3,16 +3,16 @@ import re
 def parse_parent(path):
     with open(path, "r") as f:
         return f.readlines()
-    
+
 def write_child(path, lines):
     with open(path, "w") as f:
         f.writelines(lines)
-        
+
 def modify_children(lines, file_dict):
     for key, value in file_dict.items():
         lines[0] = f"FROM {value}\n"
         write_child(f"{key}/Dockerfile", lines)
-        
+
 def modify_lines(lines, term, replacement):
     return [line.replace(term, replacement) for line in lines]
 
@@ -24,17 +24,18 @@ def main():
                  }
     neural_dict = {"tensorflow-jupyter-cpu": "jupyter/tensorflow-notebook",
                    "tensorflow-jupyter-gpu": "coreyhanson/anacuda-tensorflow"
+                   "pytorch-jupyter-gpu": "coreyhanson/anacuda-pytorch"
                   }
-    
+
     base_lines = parse_parent("base-Dockerfile")
     appened_neural_lines = parse_parent("neural-network-append")
-    
+
     neural_lines = base_lines + appened_neural_lines
-    
+
     modify_children(base_lines, file_dict)
     modify_children(neural_lines, neural_dict)
-    
-    
+
+
     print("Files written")
 
 if __name__ == "__main__":
